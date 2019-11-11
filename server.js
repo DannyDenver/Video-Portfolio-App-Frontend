d79461ed4de8a3d733b8bb60e5a5f688db608809
DannyDenver <djt09d@fulbrightmail.org>
2019-11-11 10:50:54 -0700

    update server.js

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-// app.use(express.static(__dirname + '/dist/video-portfolio-app'));
-app.use(express.static('build'));
+app.use(express.static(__dirname + '/dist/video-portfolio-app'));
+// app.use(express.static('build'));
 
 // Send all requests to index.html
-app.get('*', function (req, res) {
-  // const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile('index.html');
-});
-
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/video-portfolio-app/index.html'));
+// app.get('*', function (req, res) {
+//   const index = path.join(__dirname, 'build', 'index.html');
+//   res.sendFile(index);
 // });
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/video-portfolio-app/index.html'));
+});
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
