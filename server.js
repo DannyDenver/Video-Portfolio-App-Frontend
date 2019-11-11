018d8be20a6a39367066e929e04aa596427c723d
DannyDenver <djt09d@fulbrightmail.org>
2019-11-11 10:25:42 -0700

    move server.js file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/video-portfolio-app'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/video-portfolio-app/index.html'));
 });
 
 // default Heroku PORT
