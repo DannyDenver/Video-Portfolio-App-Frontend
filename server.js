7f2f3cb35e909732e82129b1336b5943f944dacf
DannyDenver <djt09d@fulbrightmail.org>
2019-11-11 10:45:18 -0700

    update server.js

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/video-portfolio-app'));
+// app.use(express.static(__dirname + '/dist/video-portfolio-app'));
+app.use(express.static('build'));
 
 // Send all requests to index.html
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
+  // const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile('index.html');
 });
 
 // app.get('/*', function(req, res) {
