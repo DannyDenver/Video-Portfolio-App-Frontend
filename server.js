b0cbb1cc565e15e8eb700f9ff9119eba2d963048
DannyDenver <djt09d@fulbrightmail.org>
2019-11-11 10:35:41 -0700

    use relative file paths in server.js

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const app = express();
 app.use(express.static(__dirname + '/dist/video-portfolio-app'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/video-portfolio-app/index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
+
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/video-portfolio-app/index.html'));
+// });
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
