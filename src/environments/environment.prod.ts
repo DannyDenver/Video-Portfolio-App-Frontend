6fc68a77b43110f02f814ed63b44801d70e17969
DannyDenver <djt09d@fulbrightmail.org>
2020-01-20 11:20:34 -0700

    change backend api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiServerUrl: 'https://video-portfolio-backend.herokuapp.com',
+  apiServerUrl: 'https://co9fey3jkg.execute-api.us-east-1.amazonaws.com/dev',
   auth0: {
     url: 'dannydenver', // the auth0 domain prefix
     audience: 'videoportfolio', // the audience set for the auth0 app
