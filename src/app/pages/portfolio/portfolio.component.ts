e1deba7799fc8e04b64f7de62563d3e27b957208
DannyDenver <djt09d@fulbrightmail.org>
2020-01-28 10:31:52 -0700

    fix same component routing bug

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Videographer } from 'src/app/shared/models/videographer';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { VideographerService } from 'src/app/services/videographer.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { VideosService } from 'src/app/services/videos.service';
@@ -10,12 +10,12 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 import { ConfirmationDialog } from 'src/app/core/confirmation-dialog/confirmation-dialog.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EmailService } from 'src/app/services/email.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PortfolioComponent implements OnInit {
   emailForm: FormGroup;
@@ -33,30 +33,29 @@ export class PortfolioComponent implements OnInit {
     public auth: AuthService,
     private sanitizer: DomSanitizer,
     public dialog: MatDialog,
-    private cd: ChangeDetectorRef,
     private emailService: EmailService
   ) { }
 
   ngOnInit() {
-    this.name = this.route.snapshot.paramMap.get('name');
-
-    if (this.name === 'your-portfolio') {
-      this.name = this.auth.activeUserId();
-    }
-
-
-    this.videographerService.getVideographer(this.name).subscribe(videogoo => {
-      this.videographer = videogoo
-      this.cd.detectChanges();
-    });
-
-    this.videosService.getVideos(this.name).subscribe((videos: Video[]) => {
-      this.videos = videos;
-      this.cd.detectChanges();
-    });
-
-    this.emailForm = this.fb.group({
-      email: ["", Validators.required]
+    this.route.paramMap.pipe(map((params: ParamMap) => params.get('name'))).subscribe(name => {
+      if (name === 'your-portfolio') {
+        this.name = this.auth.activeUserId();
+      } else {
+        this.name = name;
+      }
+  
+  
+      this.videographerService.getVideographer(this.name).subscribe(videogoo => {
+        this.videographer = videogoo
+      });
+  
+      this.videosService.getVideos(this.name).subscribe((videos: Video[]) => {
+        this.videos = videos;
+      });
+  
+      this.emailForm = this.fb.group({
+        email: ["", Validators.required]
+      })
     })
   }
 
@@ -82,7 +81,6 @@ export class PortfolioComponent implements OnInit {
         const userId = this.auth.activeUserId();
         this.videosService.deleteVideo(userId, video.id).subscribe((deletedUrl: string) => {
           this.videos = this.videos.filter(vid => vid.id !== video.id);
-          this.cd.detectChanges();
         })
       }
     });
@@ -94,8 +92,6 @@ export class PortfolioComponent implements OnInit {
 
   subscribe() {
     const emailAddress = this.emailForm.get('email').value.trim();
-
     this.emailService.verifyEmail(emailAddress).subscribe(x => console.log(x));
-
   }
 }
