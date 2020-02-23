a873983f45ba932d698dfecd999edd84e479c575
DannyDenver <djt09d@fulbrightmail.org>
2020-02-23 16:09:11 -0700

    no www

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
     url: 'dannydenver', // the auth0 domain prefix
     audience: 'videoportfolio', // the audience set for the auth0 app
     clientId: 'Qz3CL88tGpZYJQFqXQNjdzgXW0QCmtcp', // the client id generated for the auth0 app
-    callbackURL: 'http://www.reel-people.com', // the base url of the running ionic application. 
+    callbackURL: 'http://reel-people.com', // the base url of the running ionic application. 
   }
 };
