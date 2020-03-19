fab9872dc33c1f5333b9c831aeefde851879c4e1
DannyDenver <djt09d@fulbrightmail.org>
2020-03-19 14:47:04 -0600

    remove unused file references in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './core/header/header.component';
-import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { AuthService } from './services/auth.service';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-
 import { VideosService } from './services/videos.service';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthErrorHandler } from './services/auth-error-handler';
